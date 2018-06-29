d1512fdbc85b2d3570b7ad5b75bf3c93f81694ca
Josh Stuible <shrewdlove@gmail.com>
2018-06-29 14:52:24 -0700

    Branching narratives now end with a back button

--- _js/branching.js
+++ _js/branching.js
@@ -8,12 +8,20 @@ function initializeBranching() {
     setBackground($('.narrative-primary').data('background'));
     showMenu();
 
+    var answerCount = 0;
+
     $('.narrative-answer').click(function(){
         console.log('ya clicked: ' + $(this).attr('href'));
         var nextQuestion = $(this).attr('href');
         setBackground($('.question-container[data-narrative="' + nextQuestion + '"]').data('background'));
         $('.question-container').hide();
         $('.question-container[data-narrative="' + nextQuestion + '"]').show();
+        answerCount++;
+
+        if(answerCount > 1){
+            $('.narrative-back').show();
+        }
+
         return false;
     });
 }
