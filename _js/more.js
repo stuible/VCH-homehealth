f95a049946a759820323a19a0eee9ee68e5e6a9c
Josh Stuible <shrewdlove@gmail.com>
2018-07-31 15:28:12 -0700

    Quiz is now functional and provides feedback

--- _js/more.js
+++ _js/more.js
@@ -2,7 +2,7 @@ function instantiateMore() {
 
     showMenu();
 
-    $('.quiz.button').click(function(){
+    $('.button-container').on("click",'.quiz.button.submit', function(){
         //Check if user didn't make a selection
         if(!$("input[name=multiple-select-quiz]").is(':checked')){
             vex.dialog.alert("Please chose at least one answer");
@@ -25,13 +25,36 @@ function instantiateMore() {
             });
             if(incorrectAnwers.length > 0){
                 console.log('YOU FAILED');
-                $('.quiz.feedback').addClass('incorrect');
+                // $('.quiz.feedback').addClass('incorrect');
+                showQuizScreen('.quiz', 'incorrect', 'Wrong', 'You were wrong', 'again', 'Try Again');
             }
             else {
                 console.log('YOU NAILED IT');
-                $('.quiz.feedback').addClass('correct');
+                // $('.quiz.feedback').addClass('correct');
+                showQuizScreen('.quiz', 'correct', 'Correct', 'You was right', 'back', 'Back');
             }
         }
     });
 
+    $('.button-container').on("click",'.quiz.button.again', function(){
+        hideQuizScreen('.quiz', 'incorrect', 'again');
+    });
+    $('.button-container').on("click",'.quiz.button.back', function(){
+        hideQuizScreen('.quiz', 'correct', 'back');
+    });
+    
+
+}
+
+function showQuizScreen(quiz, screen, title, description, button, buttonText){
+
+    $(quiz).children('.response-title').text(title);
+    $(quiz).children('.response-description').text(description);
+    $(quiz).children('.quiz.feedback').addClass(screen);
+    $(quiz).children('.quiz.button.submit').removeClass('submit').addClass(button).text(buttonText);
+}
+
+function hideQuizScreen(quiz, screen, button){
+    $(quiz).children('.quiz.feedback').removeClass(screen);
+    $(quiz).children('.quiz.button.' + button).removeClass(button).addClass('submit').text('Submit');
 }
